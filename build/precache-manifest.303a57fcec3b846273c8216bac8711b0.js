51f88cbe3f1668b37663cd3fb6af1fb56c9817a4
balaji833 <balajiee4@gmail.com>
2025-04-11 14:30:48 +0530

    Initial commit

---
+++ build/precache-manifest.303a57fcec3b846273c8216bac8711b0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c789e6a8ca09089ca276b499765b346",
+    "url": "/balaji833/Anna-University.git/index.html"
+  },
+  {
+    "revision": "7be33650cbd37269f890",
+    "url": "/balaji833/Anna-University.git/static/css/2.b0aab389.chunk.css"
+  },
+  {
+    "revision": "b870c26da3aad0f7a36c",
+    "url": "/balaji833/Anna-University.git/static/css/main.3330e8aa.chunk.css"
+  },
+  {
+    "revision": "7be33650cbd37269f890",
+    "url": "/balaji833/Anna-University.git/static/js/2.bbe0f90a.chunk.js"
+  },
+  {
+    "revision": "f0e865dd2f7a07eede4a66d0e2e34530",
+    "url": "/balaji833/Anna-University.git/static/js/2.bbe0f90a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b870c26da3aad0f7a36c",
+    "url": "/balaji833/Anna-University.git/static/js/main.dbf20a55.chunk.js"
+  },
+  {
+    "revision": "17a91f56156e6acdbf9c",
+    "url": "/balaji833/Anna-University.git/static/js/runtime-main.e3e1e237.js"
+  },
+  {
+    "revision": "cb61e22e8eda81e11f5bdac80b74dd0f",
+    "url": "/balaji833/Anna-University.git/static/media/Anna.cb61e22e.jpg"
+  },
+  {
+    "revision": "5036072d983d0ee230f60be146235957",
+    "url": "/balaji833/Anna-University.git/static/media/back.5036072d.jpg"
+  },
+  {
+    "revision": "15cd8d8a21ccd77ffe9e19a5e747b38d",
+    "url": "/balaji833/Anna-University.git/static/media/logo.15cd8d8a.png"
+  },
+  {
+    "revision": "347a2951f282652734399ebba69d99a3",
+    "url": "/balaji833/Anna-University.git/static/media/mit.347a2951.jpg"
+  },
+  {
+    "revision": "dadf0d81d3704e30b208410f04b9aebf",
+    "url": "/balaji833/Anna-University.git/static/media/slide.dadf0d81.jpeg"
+  },
+  {
+    "revision": "d6c27747ae9114ff793a96d0c3e1e65b",
+    "url": "/balaji833/Anna-University.git/static/media/univ1.d6c27747.jpg"
+  },
+  {
+    "revision": "72e77e523487455fd0bd3755fc754793",
+    "url": "/balaji833/Anna-University.git/static/media/univ2.72e77e52.jpg"
+  },
+  {
+    "revision": "855ccebd1d6990ae5378f449656d3ab2",
+    "url": "/balaji833/Anna-University.git/static/media/univ3.855ccebd.jpg"
+  },
+  {
+    "revision": "a55396e744c2b0570619e81b8981f4ca",
+    "url": "/balaji833/Anna-University.git/static/media/univ4.a55396e7.jpeg"
+  },
+  {
+    "revision": "51993ab2e84c6964284079265d8aa253",
+    "url": "/balaji833/Anna-University.git/static/media/univ5.51993ab2.jpg"
+  },
+  {
+    "revision": "c349c5d3dade24771a2bda197b2b1964",
+    "url": "/balaji833/Anna-University.git/static/media/univ6.c349c5d3.jpg"
+  },
+  {
+    "revision": "11698e4cfd124490535fcb102a9a51e3",
+    "url": "/balaji833/Anna-University.git/static/media/univ7.11698e4c.jpg"
+  },
+  {
+    "revision": "2103467acf6cbd57dd2a1fa319f23be8",
+    "url": "/balaji833/Anna-University.git/static/media/univ8.2103467a.jpg"
+  },
+  {
+    "revision": "59de8bffecbfe4bff86868f5bd403a10",
+    "url": "/balaji833/Anna-University.git/static/media/univ9.59de8bff.jpg"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
-// ✅ Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// ✅ Optional Bootstrap JS (for dropdowns, modals, etc.)
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
